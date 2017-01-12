7840b762acccf70e7baacca520d8261a18fd1768
mariyaeggs <mariyawilson@live.com>
2017-01-12 17:48:24 -0500

    Initial commit

---
+++ LinkedList.h
@@ -0,0 +1,59 @@
+//----- LinkedList.h -----
+#include <iostream>
+using namespace std;
+
+#ifndef LAB1_EGGENSPERGERMARIYA_LINKEDLIST_H
+#define LAB1_EGGENSPERGERMARIYA_LINKEDLIST_H
+/**
+ * Title: Lab 1 : Linked List
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2 on 1/11/2017
+ *
+ * Header file defines the data for a program which
+ * asks a user to enter a text string and display
+ * the number of substrings that start with A
+ * and end with B.
+ *
+ * @author Mariya Eggensperger
+*/
+typedef int ElementType;
+
+class LinkedList
+{
+public:
+    LinkedList();   // constructor
+    ~LinkedList();  // destructor
+    LinkedList(const LinkedList & original); //copy constructor
+    void insert(ElementType item, int pos);
+    void erase(ElementType item);
+
+    void display(ostream & out) const;
+    /*--------------------------------------------------------------------
+    Display the contensts of this LinkedList.
+    Precondition: ostream out is open
+    Postcondition: Elements of this LinkedList have been output to out.
+    --------------------------------------------------------------------*/
+
+private:
+    class Node
+    {
+    public:
+        ElementType data;
+        Node * next;
+        //------ Node OPERATIONS
+        //-- Default constrctor: initializes next member to
+        Node()
+                : next(NULL)
+        { }
+        //-- Explicit-value constructor: initializes data member to dataValue
+        //-- and next member to 0
+        Node(ElementType dataValue)
+                : data(dataValue), next(NULL)
+        { }
+    };
+    Node * first;
+    int mySize;
+};
+#endif //LAB1_EGGENSPERGERMARIYA_LINKEDLIST_H
---
+++ Linked_List_Tester.cpp
@@ -0,0 +1,24 @@
+//----- Linked_List_Tester.cpp -----
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#include "LinkedList.h"
+/**
+ * Title: Lab 1 : Linked List
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2 on 1/11/2017
+ *
+ * The tester for a program that asks
+ * a user to enter a text string and display
+ * the number of substrings that start with A
+ * and end with B.
+ *
+ * @author Mariya Eggensperger
+*/
+int main() {
+   std::cout << "Hello, World!" << std::endl;
+   return 0;
+}
