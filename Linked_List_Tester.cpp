0ba3622fafdf024c7da05c447aeda4932b4bb11b
mariyaeggs <mariyawilson@live.com>
2017-01-12 17:55:31 -0500

    Add tester program to all files.

--- Linked_List_Tester.cpp
+++ Linked_List_Tester.cpp
@@ -19,6 +19,48 @@ using namespace std;
  * @author Mariya Eggensperger
 */
 int main() {
-   std::cout << "Hello, World!" << std::endl;
-   return 0;
+      // Test the class constructor
+      LinkedList intList;
+      cout << "Constructing intList\n";
+
+      // Test insert()
+      intList.insert(100, 0);
+      intList.display(cout);
+      cout << endl;
+
+      intList.insert(200, 0);
+      intList.display(cout);
+      cout << endl;
+
+      intList.insert(300, 1);
+      intList.display(cout);
+      cout << endl;
+
+      intList.insert(400, 1);
+      intList.display(cout);
+      cout << endl;
+
+      intList.insert(500, 3);
+      intList.display(cout);
+      cout << endl;
+
+      // Test destructor
+      {
+         LinkedList anotherList;
+         for (int i = 0; i < 10; i++)
+         {
+            anotherList.insert(100*i, i);
+         }
+         cout << "\nThis is another list\n";
+         anotherList.display(cout);
+      }
+
+      // Test erase
+      intList.erase(1);
+      intList.erase(1);
+      cout << "\n\nTwo items are erased from the first list\n";
+      intList.display(cout);
+      cout << endl;
+
+
 }
